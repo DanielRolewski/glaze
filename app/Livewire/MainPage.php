c69615a0235d06ed2b0c738bdd68a0c5839ef856
Daniel Rolewski <daniel_rolewski@outlook.com>
2024-03-22 20:28:45 +0100

    Make new RoomForm model

--- app/Livewire/MainPage.php
+++ app/Livewire/MainPage.php
@@ -7,9 +7,7 @@ use Livewire\Component;
 class MainPage extends Component
 {
     public $something = "something in Livewire";
-    public function displaySomething() {
 
-    }
     public function render()
     {
         return view('livewire.main-page');
---
+++ resources/views/livewire/room-form.blade.php
@@ -0,0 +1,5 @@
+<form wire:submit="save">
+    <input type="text" wire:model="roomName">
+    <input type="text" wire:model="nickName">
+    <buttom type="submit">Create Room</buttom>
+</form>
