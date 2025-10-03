4ccf0a74c99b0eb997a95ed972b47afe53271102
Daniel Rolewski <daniel_rolewski@outlook.com>
2025-10-03 14:48:30 +0200

    start project from scratch with Vue.js frontend

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Livewire\MainPage;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', MainPage::class);
+Route::get('/', function () {
+    return view('welcome');
+});
